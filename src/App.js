b07c840e135e1026835f78bb6c14c47ff3b81735
imed ben kalia <imedrugby2306@gmail.com>
2021-04-16 00:29:24 +0100

    complete two hero bar and start on About section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import HeroSection from "./components/HeroSection"
+import LeftBar from "./components/LeftBar";
 import Navbar from "./components/Navbar"
+import RightBar from "./components/RightBar";
+import Hidden from '@material-ui/core/Hidden';
 import "./style.css"
 
 
@@ -9,6 +12,11 @@ function App() {
     <>
       <div className="root">
         <Navbar />
+        <Hidden xsDown>
+          <LeftBar />
+          <RightBar />
+        </Hidden>
+        
           <main>
             <HeroSection />
           </main>
---
+++ src/components/RightBar.js
@@ -0,0 +1,71 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyle = makeStyles((theme) => ({
+  Bar: {
+    position: "fixed",
+    bottom: 0,
+    right: 40,
+    left: "auto",
+    zIndex: 10,
+    color: "#a8b2d1",
+    "& ul": {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: 0,
+        padding: 0,
+        listStyle: "none",
+        
+        "& li": {
+            padding: 15,
+            transition: "all 0.25s cubic-bezier(0.645,0.045,0.355,1)",
+            "&:hover":{
+              transform: "translateY(-5px)",
+              "& a": {
+                color: "rgb(100, 255, 218)",
+              }
+            }
+        },
+        
+        "& li:last-child": {
+            paddingBottom: 30,
+        },
+        "&::after": {
+            content: '""',
+            display: "block",
+            width: 1,
+            height: 90,
+            margin: "0px auto", 
+            backgroundColor: "rgb(168, 178, 209)",       
+        },
+        "& li a svg": {
+            height: 24,
+            width: 24,
+        },
+        "& li a": {
+            color: "#a8b2d1",
+            textDecoration: "none",
+            writingMode: "vertical-rl",
+            fontSize: 15,
+            letterSpacing: "0.1em",
+        }
+    },
+  },
+}));
+
+function RightBar() {
+  const classes = useStyle();
+  return (
+    <div className={classes.Bar}>
+      <ul>
+        <li>
+          <a href="">
+            imed.ben.kalia@gmail.com
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default RightBar;
