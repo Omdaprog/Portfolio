5b9cf321b912ef7b7b5d3049a5b6887ea2823ae9
imed ben kalia <imedrugby2306@gmail.com>
2021-04-17 03:50:38 +0100

    work on projectList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import HeroSection from "./components/HeroSection"
+import HeroSection from "./components/sections/HeroSection"
 import LeftBar from "./components/LeftBar";
 import Navbar from "./components/Navbar"
 import RightBar from "./components/RightBar";
 import Hidden from '@material-ui/core/Hidden';
 import "./style.css"
-import AboutSection from "./components/AboutSection";
+import AboutSection from "./components/sections/AboutSection";
+import ProjectSection from "./components/sections/ProjectSection";
 
 
 function App() {
@@ -17,10 +18,10 @@ function App() {
           <LeftBar />
           <RightBar />
         </Hidden>
-        
           <main>
             <HeroSection />
             <AboutSection />
+            <ProjectSection />
           </main>
       </div>
     </>
